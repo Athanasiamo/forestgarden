7ef84ebdbedc6dc100a66ee7e847e5d277f91d71
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-11-12 10:00:27 +0100

    add informatino about nitrogen

--- app.R
+++ app.R
@@ -10,10 +10,37 @@ ui <- dashboardPage(skin = "green",
     dashboardBody(
         # Boxes need to be put in a row (or column)
         fluidRow(
+            column(12,
+                   tags$h1("Nitrogen needs of a garden"),
+                   tags$p("If you want a garden that more or less sustains itself, 
+                   you will need to think about the necessary nutrients your plants need.
+                   Trees and shrubs need nitrogen to grow, be healthy, and bear fruit.
+                   ")),
             infoBox(title = "Tree area", htmlOutput("treeArea"), 
-                    fill=TRUE, icon = icon("tree")),
+                    fill=TRUE, color = "green", icon = icon("tree")),
             infoBox(title = "Nitrogen need", textOutput("nitro"), 
-                    fill=TRUE, icon = icon("atom")),
+                    fill=TRUE,color = "yellow", icon = icon("atom")),
+            column(12,
+                   tags$h2("Your garden"),
+                   tags$p("Below you can set the number of trees and shrubs in
+                          your garden. This will create a list for you to fill out
+                          for each tree or shrub, indicating the radius of the 
+                          trees fully grown crown, and its nitrogen need.
+                          Once you have filled out the list, you can scroll back to the top,
+                          where the tree crown area and nitrogen need for your entire garden
+                          will have been calculated."),
+                   tags$p("Since different types of trees require different amounts of nitrogen,
+                          there are three tiers of nitrogen need you may indicate. You should
+                          only here add trees that require nitrogen, not your trees and shrubs 
+                          that are nitrogen fixators them selves."),
+                   tags$p("These are rules-of-thumb and you might have trees or plants that require
+                          more or less than we here allow you to set. The final calculation
+                          should give you an idea of what your gardens need is as a whole."),
+                   tags$ul(tags$b("Nitrogen tiers"),
+                           tags$li("Low - Non-fruit bearing - 4g/m", tags$sup("2")),
+                           tags$li("Medium - Fruit bearing - 8g/m", tags$sup("2")),
+                           tags$li("High - Annual vegetables - 15g/m", tags$sup("2")))
+                   ),
             box(sliderInput("ntrees", "Number of trees", value = 5, 
                             min = 1, max = 50, round = TRUE)),
             box(uiOutput("sliders" ))
